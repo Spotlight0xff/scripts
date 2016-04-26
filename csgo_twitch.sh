938decc06c5bebed07030923bb710010a5041a69
spotlight0xff <spotlight0xff@gmail.com>
2016-04-26 14:44:23 +0200

    added csgo-twitch and twitch scripts (using zenity)

---
+++ csgo_twitch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+MY_PATH="`dirname \"$0\"`"
+
+(streams=$(python2 $MY_PATH/streams.py -d '`')
+echo "$streams" > /tmp/.streams
+) | zenity --progress --pulsate --auto-close --text="Fetch CS:GO Streams"
+
+if [ $? == 1 ] ; then exit; fi
+
+i=-1
+while read line;
+do
+    i=$(($i+1))
+    name=$(echo $line | cut -d '`' -f1)
+    viewers=$(echo $line | cut -d '`' -f2)
+    array[$i]=$(echo "$name $viewers")
+done < /tmp/.streams
+
+rm /tmp/.streams
+
+sel=$(zenity --list --title="Choose the Livestream" \
+    --column="Name" --column="Viewers"\
+    ${array[*]})
+if [ ! $sel ]; then exit; fi
+$MY_PATH/twitch.sh $sel
+
---
+++ twitch.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+if [[ $1 ]]; then
+    username=$1
+else
+    username=$(zenity --entry --title="Watch Twitchstreams" --text="Enter the Twitch.tv-Stream:")
+    if [ ! $username ]; then exit; fi
+fi
+
+same_qual=false
+
+while true
+do
+    if [ $same_qual == false ]; then
+
+        (output=$(livestreamer twitch.tv/$username)
+        echo $output > /tmp/livestream_info
+        )| zenity --progress --pulsate --auto-close --text="Fetch Livestream information"
+        if [ $? == 1 ]; then exit; fi
+        output=$(cat /tmp/livestream_info)
+
+        if [[ "$output" == *"source (best, worst)"* ]]; then
+            qualities[0]="source"
+            qual_count=0
+        else
+            qual_count=$(echo $output|grep -o "," | wc -l)
+            let qual_count=qual_count+1
+        fi
+        for ((i=1; i <= $qual_count; i++))
+        do
+            qualities[$i]=$(echo $output| cut -d':' -f2| cut -d',' -f $i|cut -d' ' -f2)
+        done
+
+        in=$(zenity --list --height=300 --title="Choose Quality" --column="Quality" ${qualities[*]})
+        if [ ! $in ]; then exit; fi
+    fi
+
+    livestreamer -Q de.twitch.tv/$username $in
+
+#    rr=$(zenity --question --text "Restart Stream?")
+    if ! zenity --question --title=" " --text="Restart Stream?"; then
+        break
+    fi
+
+ #   rr_qual=$(zenity --question --text "Same Quality?")
+    if zenity --question --title="   " --text="Same Quality?"; then
+        same_qual=true
+    else
+        same_qual=false
+    fi
+done
