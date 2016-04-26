e993f31c367f9d0b3c5fb0878815552a072e531a
spotlight0xff <spotlight0xff@gmail.com>
2016-04-26 11:43:43 +0200

    added dzen2-based bar (for workspaces)
    dzen2 is patched to support bottom docking (-dock b)

---
+++ sleep_dz.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+CURDIR=`dirname $0`
+pkill -f i3-wsbar
+pkill -f dzen2
+sleep 1
+$CURDIR/i3-wsbar | $CURDIR/dzen2 -dock b -e 'button5=exec:i3-msg workspace next;button4=exec:i3-msg workspace prev'
