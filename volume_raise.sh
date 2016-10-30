ec69078d6a34c9818b8d3bb3dbc8ea5ee6e4d3af
Spotlight0xff <spotlight0xff@gmail.com>
2016-10-30 16:23:46 +0100

    card independent volume changes

--- volume_raise.sh
+++ volume_raise.sh
@@ -1,3 +1,3 @@
 amixer -q -M set Master 5%+
-vol=$(amixer -c 0 get Master | grep Mono: | cut -d " " -f6 | grep -o -E '[0-9]*')
+vol=$(amixer get Master | grep Mono: | cut -d " " -f6 | grep -o -E '[0-9]*')
 volnoti-show $vol
