a052be5d1be2ed0c99d16501f3c9f561031799d4
André Merboldt <andre.merboldt@rwth-aachen.de>
2017-01-17 15:11:05 +0100

    allow also 1280x1024 conky config

--- conky_sel.sh
+++ conky_sel.sh
@@ -4,8 +4,11 @@ width=$(xrandr | grep '*' | cut -d'x' -f1| head -1 |tr -d '[[:space:]]')
 
 if [[ $width == 1366 ]]; then
     dir="laptop"
+elif [[ $width == 1280 ]]; then
+    dir="1024"
 else
     dir="fullhd"
 fi
+
 conky -c ~/.conky/$dir/conky_overlay.conf &
 conky -c ~/.conky/$dir/conky_bar.conf &
