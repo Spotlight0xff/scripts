e66ce0aa4cdd3bbd0717ffd4069db2e2e209b80e
André Merboldt <andre.merboldt@rwth-aachen.de>
2017-02-14 22:04:43 +0100

    show volnoti async

--- volume_lower.sh
+++ volume_lower.sh
@@ -1,4 +1,4 @@
 amixer -q -M set Master 5%-
 #vol=$(amixer get Master | grep Mono: | cut -d " " -f6 | grep -o -E '[0-9]*')
 vol=$(amixer get Master | grep -m1 -o "[0-9]*\%" | grep -o "[0-9]*")
-volnoti-show $vol
+volnoti-show $vol &
