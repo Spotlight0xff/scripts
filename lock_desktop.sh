052dd9085a5f1101ebb973ecae46e4c974801f6f
André Merboldt <andre.merboldt@rwth-aachen.de>
2017-02-14 22:04:09 +0100

    change lockscreen image for laptop

--- lock_desktop.sh
+++ lock_desktop.sh
@@ -2,9 +2,9 @@
 CURDIR=`dirname $0`
 width=$(xrandr | grep '*' | cut -d'x' -f1| tr -d '[[:space:]]')
 if [[ $width == 1366 ]]; then
-    file="$CURDIR/wallpaper-1107219_laptop.png"
+  file="$CURDIR/wallhaven-143449_laptop.png"
 else
-    file="$CURDIR/wallpaper-1107219.png"
+  file="$CURDIR/wallpaper-1107219.png"
 fi
 
 i3lock -i $file
