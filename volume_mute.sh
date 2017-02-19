9cae4d00623d022c974acd5b7d8fb747f29eb706
Spotlight0xff <spotlight0xff@gmail.com>
2017-02-19 18:34:39 +0100

    alsa -> pulseaudio

--- volume_mute.sh
+++ volume_mute.sh
@@ -1,8 +1,13 @@
-amixer -q set Master toggle
-mute=$(amixer -c 0 get Master | grep Mono: | grep -o -E '\[(on|off)\]')
-if [[ "$mute" == "[off]" ]]; then
-    volnoti-show -m
+#!/bin/bash
+
+SINK=$(pactl list short | grep RUNNING | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,')
+pactl set-sink-mute $SINK toggle
+
+mute=$(pactl list sinks | grep -A15 -P "\#$SINK" | grep Mute | cut -d' ' -f2)
+if [[ "$mute" == "yes" ]]; then
+  volnoti-show -m
 else
-    vol=$(amixer -c 0 get Master | grep Mono: | cut -d " " -f6 | grep -o -E '[0-9]*')
-    volnoti-show $vol
+  VOLUME=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
+    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+  volnoti-show $VOLUME
 fi
--- volume_raise.sh
+++ volume_raise.sh
@@ -1,4 +1,6 @@
-amixer -q -M set Master 5%+
-#vol=$(amixer get Master | grep Mono: | cut -d " " -f6 | grep -o -E '[0-9]*')
-vol=$(amixer get Master | grep -m1 -o "[0-9]*\%" | grep -o "[0-9]*")
-volnoti-show $vol &
+SINK=$(pactl list short | grep RUNNING | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,')
+
+pactl set-sink-volume $SINK +5%
+VOLUME=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
+  head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+volnoti-show $VOLUME &
