e1007461052b990b17394a483d5fa6353d1650fe
Spotlight0xff <spotlight0xff@gmail.com>
2017-03-08 19:02:59 +0100

    script to set settings for the ss rival

---
+++ rival_settings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# requires to have https://github.com/flozz/rivalcfg installed.
+
+rivalcfg --sensitivity1=1600
+rivalcfg --logo-color=#4444cc # light blue
+rivalcfg --wheel-color=#4444cc
