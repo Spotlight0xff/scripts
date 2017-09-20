7b31b861f88ae280b7d823ecac00857107723e2f
spotlight0xff <spotlight0xff@gmail.com>
2017-09-20 21:46:34 +0200

    remove zenity dependency (not used, ugly)

--- twitch.sh
+++ twitch.sh
@@ -2,33 +2,14 @@
 
 MY_PATH="`dirname \"$0\"`"
 
-if [[ $1 ]]; then
-    username=$1
-else
-    username=$(zenity --entry --title="Watch Twitchstreams" --text="Enter the Twitch.tv-Stream:")
-    if [ ! $username ]; then exit; fi
+if [[ ! $1 ]]; then
+  echo "provide a twitch stream name!"
+  exit
 fi
 
-same_qual=false
+username="$1"
 
-while true
-do
-    if [ $same_qual == false ]; then
-      in=$($MY_PATH/livestreamer_parse_qual.sh $username| rofi -dmenu -p "Quality: ")
-      if [ ! $in ]; then exit; fi
-    fi
+in=$($MY_PATH/streamlink_parse_qual.sh $username| rofi -dmenu -p "Quality: ")
+if [[ ! $in ]]; then exit; fi
 
-    livestreamer -Q de.twitch.tv/$username $in
-
-#    rr=$(zenity --question --text "Restart Stream?")
-    if ! zenity --question --title=" " --text="Restart Stream?"; then
-        break
-    fi
-
- #   rr_qual=$(zenity --question --text "Same Quality?")
-    if zenity --question --title="   " --text="Same Quality?"; then
-        same_qual=true
-    else
-        same_qual=false
-    fi
-done
+streamlink -Q de.twitch.tv/$username $in
