0c227eb3ec40f9df66d94ecb4f22c1773384a310
Spotlight0xff <spotlight0xff@gmail.com>
2017-11-28 14:34:06 +0000

    updated swaylock

--- lock_desktop_sway.sh
+++ lock_desktop_sway.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 CURDIR=`dirname $0`
-file="$CURDIR/lock.png"
-
-#swaylock --indicator-radius 50 --indicator-thickness 2 --insidecolor 80808080 --keyhlcolor 000000a0 --linecolor 80808080 --ringcolor 80808000 -r -t -i $file
-swaylock --indicator-radius 50 --indicator-thickness 2 --insidecolor a0a0a0 --keyhlcolor 000000 --linecolor a0a0a0 --ringcolor a0a0a000 -r -t -i $file
+FILE="$CURDIR/lock.png"
+BACKGROUND="355C7D"
+swaylock --indicator-radius 50 --indicator-thickness 4 --insidecolor $BACKGROUND --keyhlcolor 000000 --linecolor $BACKGROUND --ringcolor ${BACKGROUND}00 -r -t -i $FILE
